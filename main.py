1bef3bfcf8d3e92b66db4dd8909d14e544241b5e
Mikhail <babichev.mikhail.s@gmail.com>
2023-03-30 16:26:10 +0700

    feat(): all done

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
-import time
-import numpy
 from PIL import Image
 import numpy as np
 import dash
-from dash import dcc, Output, Input
 from dash import html
-from dash.exceptions import PreventUpdate
 
-image = Image.open('D:\\geometry_figures.jpg', mode='r')
+rgb = [[46, 58, 35],
+       [255, 24, 46],
+       [255, 24, 205],
+       [165, 24, 255],
+       [43, 24, 255],
+       [251, 206, 177],
+       [24, 255, 119],
+       [24, 177, 255],
+       [225, 255, 24],
+       [120, 88, 64]]
+
+image = Image.open('D:\\black_and_white.jpg', mode='r')
 src = np.array(image)
 
 n_sum = src.shape[0] * src.shape[1]
 
-bites = [1 * i for i in range(256)]
 count = [0 * i for i in range(256)]
 
 src[:, :, 1] = src[:, :, 0]
@@ -24,7 +30,6 @@ data.save('assets\\gf.png')
 for i in range(src.shape[0]):
     for j in range(src.shape[1]):
         count[src[:, :, 0][i][j]] += 1
-
 
 in_sum = 0
 
@@ -58,23 +63,66 @@ for t in range(0, 255):
         maxSigma = sigma
         threshold = t
 
-print(threshold)
-
-image1 = Image.open('assets\\gf.png')
-src1 = np.array(image1)
+image = Image.open('assets\\gf.png')
+src = np.array(image)
 for i in range(src.shape[0]):
     for j in range(src.shape[1]):
         if int(src[:, :, 0][i][j]) <= int(threshold):
-            src1[:, :, 0][i][j] = 0
+            src[:, :, 0][i][j] = 0
         else:
-            src1[:, :, 0][i][j] = 255
-src1[:, :, 1] = src1[:, :, 0]
-src1[:, :, 2] = src1[:, :, 0]
+            src[:, :, 0][i][j] = 255
+src[:, :, 1] = src[:, :, 0]
+src[:, :, 2] = src[:, :, 0]
 
-data1 = Image.fromarray(src1)
+data = Image.fromarray(src)
 
-data1.save('assets\\bw_gf.png')
+count = [0 * i for i in range(256)]
 
+for i in range(src.shape[0]):
+    for j in range(src.shape[1]):
+        count[src[:, :, 0][i][j]] += 1
+
+data.save('assets\\bw_gf.png')
+
+
+array = np.zeros((src.shape[0] + 2, src.shape[1] + 2), dtype="uint8")
+for i in range(src.shape[0]):
+    for j in range(src.shape[1]):
+        if src[:, :, 0][i][j] == 255:
+            array[i + 1][j + 1] = 1
+
+
+def recursive(array, height, width, counter):
+    if array[height][width] == 1:
+        array[height][width] = counter
+        if array[height - 1][width] == 1:
+            recursive(array, height - 1, width, counter)
+        if array[height][width + 1] == 1:
+            recursive(array, height, width + 1, counter)
+        if array[height + 1][width] == 1:
+            recursive(array, height + 1, width, counter)
+        if array[height][width - 1] == 1:
+            recursive(array, height, width - 1, counter)
+
+
+counter = 2
+for i in range(src.shape[0]):
+    for j in range(src.shape[1]):
+        recursive(array, i + 1, j + 1, counter)
+        if array[i + 1][j + 1] == counter:
+            counter += 1
+
+for i in range(src.shape[0]):
+    for j in range(src.shape[1]):
+        for k in range(counter - 1):
+            if array[i + 1][j + 1] == k + 2:
+                src[:, :, 0][i][j] = rgb[k][0]
+                src[:, :, 1][i][j] = rgb[k][1]
+                src[:, :, 2][i][j] = rgb[k][2]
+
+data = Image.fromarray(src)
+data.save('assets\\colored_gf.png')
+print(counter - 2)
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
@@ -84,37 +132,11 @@ app.layout = html.Div(children=[
         html.Img(width='50%', id='initial_image', src='assets\\bw_gf.png'),
 
     ], style={'textAlign': 'center'}),
+    html.Div([
+        html.Img(width='50%', id='new_image', src='assets\\colored_gf.png'),
 
+    ], style={'textAlign': 'center'})
 ])
-
-
-# @app.callback(
-#     Output(component_id='initial_image', component_property='src'),
-#     Input(component_id='show-secret', component_property='n_clicks'),
-#     Input('table-editing-simple', 'data'),
-#     Input('table-editing-simple', 'columns'),
-#     prevent_initial_call=True
-# )
-#
-# def update_output(value):
-#     image1 = Image.open('assets\\bw.png')
-#     src1 = np.array(image1)
-#     if value is None:
-#         return app.get_asset_url('bw.png')
-#     for i in range(src.shape[0]):
-#         for j in range(src.shape[1]):
-#             if int(src[:, :, 0][i][j]) <= int(value):
-#                 src1[:, :, 0][i][j] = 0
-#             else:
-#                 src1[:, :, 0][i][j] = 255
-#     src1[:, :, 1] = src1[:, :, 0]
-#     src1[:, :, 2] = src1[:, :, 0]
-#
-#     data1 = Image.fromarray(src1)
-#
-#     data1.save('assets\\bw.png')
-#     return app.get_asset_url('bw.png')
-
 
 if __name__ == '__main__':
     app.run_server(debug=True)
